f75f5b9a106599d810fa0c7d1a8671cb2c17ac16
Cole Dunsby <coledunsby@gmail.com>
2015-02-11 17:49:55 -0500

    Created main activity.

--- TipCalculator/app/src/main/java/com/example/cole/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/example/cole/tipcalculator/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.cole.tipcalculator;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -14,6 +18,17 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        float tip_percentage = sharedPref.getFloat(getString(R.string.saved_tip_percentage), 0);
+
+        EditText tipPercentage = (EditText) findViewById(R.id.tipPercentage);
+        tipPercentage.setText(Float.toString(tip_percentage));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,9 +46,23 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            this.startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showSummary(View view) {
+        EditText billAmount = (EditText) findViewById(R.id.billAmount);
+        EditText tipPercentage = (EditText) findViewById(R.id.tipPercentage);
+        EditText numberOfPeople = (EditText) findViewById(R.id.numberOfPeople);
+
+        Intent intent = new Intent(this, SummaryActivity.class);
+        intent.putExtra("BILL_AMOUNT", billAmount.getText().toString());
+        intent.putExtra("TIP_PERCENTAGE", tipPercentage.getText().toString());
+        intent.putExtra("NUMBER_OF_PEOPLE", numberOfPeople.getText().toString());
+        startActivity(intent);
     }
 }
